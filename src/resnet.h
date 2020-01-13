2f7cdbc2a4366030f16575dc2a6cc42c8759db33
Adrià Arrufat <adria.arrufat@omnious.com>
2020-01-14 01:25:04 +0900

    fix formatting

--- src/resnet.h
+++ src/resnet.h
@@ -70,8 +70,7 @@ struct resnet
         dlib::repeat<nb_512, resbasicblock_512, resbasicblock_down<512,
         dlib::repeat<nb_256, resbasicblock_256, resbasicblock_down<256,
         dlib::repeat<nb_128, resbasicblock_128, resbasicblock_down<128,
-        dlib::repeat<nb_64,  resbasicblock_64,
-        input_processing<INPUT>>>>>>>>;
+        dlib::repeat<nb_64,  resbasicblock_64, input_processing<INPUT>>>>>>>>;
 
     // the resnet backbone with bottlenecks
     template<long nb_512, long nb_256, long nb_128, long nb_64, typename INPUT>
